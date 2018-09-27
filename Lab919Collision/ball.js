b5b2e4a97e8ac6729e4c8974b6adbb5f3e6f6ba0
Isaiah <BoltTheNinja@gmail.com>
2018-09-27 08:44:51 -0700

    Removed If statement & tried to lerp chaser to mouse

--- Lab919Collision/ball.js
+++ Lab919Collision/ball.js
@@ -18,21 +18,10 @@ function Ball(location, velocity, radius, col){
   //This function changes the location of the Ball
   // by adding speed to x and y
   this.update = function(){
+    var mouseLoc = createVector(mouseX, mouseY);
+    this.loc = p5.Vector.lerp(this.loc, mouseLoc, .09);
 
 
-    if(this !== attr){
-      var d = this.loc.dist(attr.loc)
-      if(d < 300){
-        var steeringForce = p5.Vector.sub( attr.loc, this.loc);
-        steeringForce.normalize(); //changes the magnitude to 1
-        steeringForce.mult(0.09); // scales the magnitude to 0.5
-        this.vel.add(steeringForce);
-      }
-      if(d < 80){
-        var steeringForce = p5.Vector.sub( this.loc, attr.loc);
-        steeringForce.normalize(); //changes the magnitude to 1
-        steeringForce.mult(0.2); // scales the magnitude to 0.5
-        this.vel.add(steeringForce);
       }
 
 
@@ -41,7 +30,7 @@ function Ball(location, velocity, radius, col){
     this.loc.add(this.vel);
 
   }
-  
+
   }
 
   // render() draws the ball at the new location
