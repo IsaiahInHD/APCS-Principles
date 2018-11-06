8db86c2df1c8f5760d686ca90a35721b3dd97352
IsaiahInHD <36487768+IsaiahInHD@users.noreply.github.com>
2018-11-06 08:47:41 -0800

    idk

--- Lab1101 JSON/sketch.js
+++ Lab1101 JSON/sketch.js
@@ -1,20 +1,50 @@
-var myData;
+var data;
 var barHeight;
 
-function setup() {
-  loadJSON("pokedex.json", gotData)
-}
 
-function gotData(jData) {
-  myData = jData;
+function setup() {
+  var cnv = createCanvas(1600, 900);
+  cnv.position((windowWidth-width)/2, 30);
+  background(20);
+  barHeight = 20;
+  loadJSON("pokedex.json", gotData);
+
+
 }
 
 function draw() {
-  for(var j = 0; j < i; j+1){
-    if(myData.pokemon[j].names >
-      myData.pokemon[j+1].names){
-        swap(j, j+1)
-      }
+  useData();
+}
+
+function gotData(jData) {
+  data = jData;
+
+}
+
+function useData() {
+  if(data){
+    bubbleSort();
+    for(var i = 0; i < data.pokemon.length; i++){
+      console.log(i + '-' + data.pokemon[i].type + 'weaknesses -' + data.pokemon[i].weaknesses)
+    }
   }
+
+}
+
+
+function bubbleSort(data) {
+  var length = data.length;
+  for(var i = 0; i < length; i++) {
+    for(var j = 0; j, (length - i - 1); j++){
+      if(items[j] > items[j+1]) {
+        var tmp = items[j];
+        items[j] = items[j+1];
+        items[j=1] = tmp;
+      }
+      
+    }
+
+  }
+
 
 }
