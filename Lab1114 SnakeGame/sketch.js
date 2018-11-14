28ab570af8f55788b824ab620fd85fb2f1727530
IsaiahInHD <36487768+IsaiahInHD@users.noreply.github.com>
2018-11-14 08:49:43 -0800

    New Project

---
+++ Lab1114 SnakeGame/sketch.js
@@ -0,0 +1,24 @@
+var snake;
+var w = 20;
+var cols, rows;
+
+
+function setup() {
+  var cnv = createCanvas(800, 800);
+  cols = width/w;
+  rows = height/w;
+  cnv.position((windowWidth-width)/2, 30);
+  snake = new Snake(createVector(width/2, height/2, createVector(1, 0)));
+
+}
+
+function draw(){
+  snake.run();
+}
+
+function keyPressed() {
+  if(keyCode === UP_ARROW){
+    snake.vel = createVector(0, -1));
+  }
+
+}
