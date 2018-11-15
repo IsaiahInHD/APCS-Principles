f5c8349aa3a593c9ac9e8455ac930c5c0b0d98d2
IsaiahInHD <36487768+IsaiahInHD@users.noreply.github.com>
2018-11-14 22:55:57 -0800

    Done w/ deadline

--- Lab1114 SnakeGame/sketch.js
+++ Lab1114 SnakeGame/sketch.js
@@ -4,7 +4,8 @@ var cols, rows;
 
 
 function setup() {
-  var cnv = createCanvas(800, 800);
+  var cnv = createCanvas(1600, 900);
+  background(20, 20, 20);
   cols = width/w;
   rows = height/w;
   cnv.position((windowWidth-width)/2, 30);
@@ -18,7 +19,16 @@ function draw(){
 
 function keyPressed() {
   if(keyCode === UP_ARROW){
-    snake.vel = createVector(0, -1));
+    snake.vel = createVector(0, -1);
+  }
+  if(keyCode === DOWN_ARROW){
+    snake.vel = createVector(0, 1);
+  }
+  if(keyCode === LEFT_ARROW){
+    snake.vel = createVector(-1, 0);
+  }
+  if(keyCode === RIGHT_ARROW){
+    snake.vel = createVector(1, 0);
   }
 
 }
